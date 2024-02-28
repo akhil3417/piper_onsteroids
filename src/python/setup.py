dfdb3c04c7e607c2f2da1c4b6d9d6999a8f3352b
Akhil <92385307+akhil3417@users.noreply.github.com>
2024-02-28 21:17:32 +0530

    Revert "Fix python install"

--- src/python/setup.py
+++ src/python/setup.py
@@ -56,8 +56,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
     ],
     keywords="rhasspy tts speech voice",
 )
